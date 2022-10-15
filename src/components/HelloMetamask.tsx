db5a539f31417c972aa3d419060b8dcbc5878379
Javier Guajardo <javier.guajardoj@gmail.com>
2022-10-15 04:23:42 +0200

    Refactor HelloMetamask

--- src/components/HelloMetamask.tsx
+++ src/components/HelloMetamask.tsx
@@ -1,24 +1,24 @@
 import { useMetamask, ConnectMetamask } from "../metamask";
 
 export default function HelloMetamask() {
-	const { user } = useMetamask();
+  const { user } = useMetamask();
 
-	return (
-		<div className="flex flex-col items-center bg-slate-100 h-screen justify-center">
-			<div className="shadow-lg text-center border border-slate-300 bg-white p-10 rounded-md">
-				{!user.isConnected ? (
-					<ConnectMetamask />
-				) : (
-					<>
-						<div className="my-1 uppercase tracking-wide">
-							Wallet connected:
-						</div>
-						<div className="my-1 tracking-widest font-extrabold">
-							{user.address}
-						</div>
-					</>
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center bg-slate-100 h-screen justify-center">
+      <div className="shadow-lg text-center border border-slate-300 bg-white p-10 rounded-md">
+        {!user.isConnected ? (
+          <ConnectMetamask />
+        ) : (
+          <>
+            <div className="my-1 uppercase tracking-wide">
+              Wallet connected:
+            </div>
+            <div className="my-1 tracking-widest font-extrabold">
+              {user.address}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
