47f1a413f5140511972d8185fe62d357f14b36d2
Javier Guajardo <javier.guajardoj@gmail.com>
2022-10-15 04:21:29 +0200

    Adding page to see requests sent

--- src/pages/sent/index.tsx
+++ src/pages/sent/index.tsx
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react";
+import { useMetamask } from "../../metamask";
+
 export default function Sent() {
-  return <div>Requests sent</div>;
+  const { contract } = useMetamask();
+  const [payments, setPayments] = useState<any[]>([]);
+  const loadToGetPaid = async () => {
+    try {
+      const response = await contract.toGetPaid();
+      setPayments(response);
+    } catch (e) {
+      console.log("Error loading payment", e);
+    }
+  };
+
+  useEffect(() => {
+    loadToGetPaid();
+  }, []);
+
+  return <div>Requests sent {payments.length}</div>;
 }
