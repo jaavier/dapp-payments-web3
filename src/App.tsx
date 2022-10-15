96263b545a454bcc74dade6102dc417a83329b11
Javier Guajardo <javier.guajardoj@gmail.com>
2022-10-15 03:20:45 +0200

    Add route and component to pay

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,25 @@ import Sent from "./pages/sent";
 import Error from "./pages/error";
 import Create from "./pages/create";
 import Container from "./components/Container";
+import Pay from "./pages/pay";
 
 export default function App() {
   return (
-    <MetamaskProvider>
-      <Container>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />}>
-              <Route index element={<Home />} />
-            </Route>
-            <Route path="received" element={<Received />} />
-            <Route path="sent" element={<Sent />} />
-            <Route path="create" element={<Create />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </BrowserRouter>
-      </Container>
-    </MetamaskProvider>
-  );
+		<MetamaskProvider>
+			<Container>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Home />}>
+							<Route index element={<Home />} />
+						</Route>
+						<Route path="received" element={<Received />} />
+						<Route path="sent" element={<Sent />} />
+						<Route path="create" element={<Create />} />
+						<Route path="pay" element={<Pay />} />
+						<Route path="*" element={<Error />} />
+					</Routes>
+				</BrowserRouter>
+			</Container>
+		</MetamaskProvider>
+	);
 }
