a0252139e33b18fab3cedc8eea09719ded0e2538
Javier Guajardo <javier.guajardoj@gmail.com>
2022-10-15 03:35:25 +0200

    Pay request

--- src/pages/pay/index.tsx
+++ src/pages/pay/index.tsx
@@ -1,3 +1,23 @@
+import { useParams } from "react-router-dom";
+
 export default function Pay() {
-  return <div>Pay Request</div>;
+  const { paymentId } = useParams();
+
+  return (
+    <div>
+      <div className="text-center text-lg mb-4 font-light uppercase tracking-wide">
+        Pay Request <span className="underline">#{paymentId}</span>
+      </div>
+      <div>
+        <div className="font-light tracking-wide flex gap-2">
+          <div className="uppercase font-bold">Receiver</div>
+          <div>0xd823Ff0182d87666e8e1eDFb4287Fd99FbDaD8C8</div>
+        </div>
+        <div className="font-light tracking-wide flex gap-4 my-2">
+          <div className="uppercase font-bold">Amount</div>
+          <div>1.3 ETH</div>
+        </div>
+      </div>
+    </div>
+  );
 }
